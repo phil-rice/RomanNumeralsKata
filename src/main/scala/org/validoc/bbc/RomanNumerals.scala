9821cc88beaf9f38732eb347c3ae0ac205ddcce4
Phil <phil.rice@iee.org>
2016-01-14 19:01:31 +0000

    Fixed error in specification, and revised Exceptions

--- src/main/scala/org/validoc/bbc/RomanNumerals.scala
+++ src/main/scala/org/validoc/bbc/RomanNumerals.scala
@@ -7,7 +7,8 @@ trait RomanNumeralGenerator {
 
 abstract class RomanNumeralException(msg: String) extends Exception(msg)
 class RomanNumeralAssertion(msg: String) extends Exception(msg)
-class RomanNumeralCannotBeNegative(msg: String) extends Exception(msg)
+class RomanNumeralCannotBeNegative(number: Int) extends Exception(s"Cannot transform numbers that are negative. This parameter was [${number}]")
+class RomanNumeralCannotBeGreaterThan3999(number: Int) extends Exception(s"Cannot transform numbers greater than 3999 to roman numerals. This parameter was [${number}]")
 
 /** Implementation of a RomanNumeralGenerator using a recursive approach */
 trait RecursiveRomanNumeralGenerator extends RomanNumeralGenerator {
@@ -16,12 +17,15 @@ trait RecursiveRomanNumeralGenerator extends RomanNumeralGenerator {
       val count = number / value
       val remainder = number % value
       string * count + generate(tail, remainder)
-    case Nil if number != 0 => throw new RomanNumeralAssertion("Software Exception: if the reference list is empty, the number should be zero")
-    case Nil                => ""
+    case Nil if number != 0 =>
+      throw new RomanNumeralAssertion(s"Software Exception: if the reference list is empty, the number should be zero.\nNumber $number\nrefencesListleft: $refernenceListLeft")
+    case Nil => ""
   }
-  def generate(number: Int): String =
-    if (number < 0) throw new RomanNumeralCannotBeNegative("Cannot have a negative roman number") else
-      generate(RomanNumeralGenerator.reference, number)
+  def generate(number: Int): String = number match {
+    case _ if number < 0    => throw new RomanNumeralCannotBeNegative(number)
+    case _ if number > 3999 => throw new RomanNumeralCannotBeGreaterThan3999(number)
+    case _                  => generate(RomanNumeralGenerator.reference, number)
+  }
 }
 
 object RomanNumeralGenerator {
